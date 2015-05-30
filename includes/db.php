6d935618beffece537b5d938f8a4414919a1345d
yarhtut <yarhtut@live.com>
2015-05-30 22:13:46 +1200

    master
    day1

---
+++ includes/db.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 21104216
+ * Date: 30/05/2015
+ * Time: 9:26 PM
+ */
+
+$db['db_host'] = "127.0.0.1";
+$db['db_username'] = 'root';
+$db['db_password'] = '';
+$db['db_dbname'] = 'yar_cms';
+
+foreach($db as $key => $value){
+    define(strtoupper($key ), $value);
+}
+
+$db = mysqli_connect(DB_HOST,DB_USERNAME,DB_PASSWORD, DB_DBNAME );
+
+if($db){
+    echo "we are connected";
+}
---
+++ includes/footer.php
@@ -0,0 +1,33 @@
+<!--
+/**
+ * Created by PhpStorm.
+ * User: 21104216
+ * Date: 30/05/2015
+ * Time: 10:08 PM
+ */-->
+
+    <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Your Website 2014</p>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+        </footer>
+
+        </div>
+        <!-- /.container -->
+
+<!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
